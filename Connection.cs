1c768f5d6f746ca9312f32a3e4ea2dbb29571fe7
winggar <b@1051productions.com>
2021-11-03 13:52:03 -0500

    added more stuff to Connection

--- Connection.cs
+++ Connection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using Renci.SshNet;
 
 namespace MultiSSH
@@ -14,7 +15,12 @@ namespace MultiSSH
         public string Password { get; set; } = null;
         public int? Port { get; set; } = null;
 
-        private SshClient Client { get; set; }
+        private SshClient Client { get; }
+
+        private Shell _shell;
+        private MemoryStream _inputStream, _outputStream, _errorStream;
+
+        private static UnicodeEncoding UnicodeEncoding { get; } = new();
 
         public Connection(string hostName, string username, string password = null, int? port = null)
         {
@@ -34,10 +40,53 @@ namespace MultiSSH
             Client = new SshClient(connectionInfo);
         }
 
+        public void CreateShell()
+        {
+            _inputStream = new MemoryStream();
+            _outputStream = new MemoryStream();
+            _errorStream = new MemoryStream();
+
+            _shell = Client.CreateShell(_inputStream, _outputStream, _errorStream);
+            _shell.Start();
+        }
+
+        public void InitShell()
+        {
+            if (_shell is null) CreateShell();
+        }
+
+        public void Write(string command) => _inputStream.Write(UnicodeEncoding.GetBytes(command));
+
+        public void Read(byte[] outputBuffer, byte[] errorBuffer)
+        {
+            _outputStream.Read(outputBuffer, 0, (int)_outputStream.Length);
+            _errorStream.Read(errorBuffer, 0, (int)_errorStream.Length);
+        }
+
+        public (string, string) ReadText(byte[] outputBuffer, byte[] errorBuffer)
+        {
+            Read(outputBuffer, errorBuffer);
+
+            return (UnicodeEncoding.GetString(outputBuffer), UnicodeEncoding.GetString(errorBuffer));
+        }
+
+        public void Flush()
+        {
+            _inputStream.Flush();
+            _outputStream.Flush();
+            _errorStream.Flush();
+        }
+
         public void Connect() => Client.Connect();
         public void Disconnect() => Client.Disconnect();
         public void Dispose() => Client.Dispose();
-        public void RunCommand(string command) => Client.RunCommand(command);
+        public void RunCommand(string commandText) => Client.RunCommand(commandText);
+        public (int, string, string) RunCommandVerbose(string commandText)
+        {
+            var command = Client.CreateCommand(commandText);
+            command.Execute();
 
+            return (command.ExitStatus, command.Result, command.Error);
+        }
     }
 }
