aa658a3c1c754249b102b4d1f9ea31df2622f53e
winggar <b@1051productions.com>
2021-11-03 11:51:16 -0500

    Started work on Connection class

---
+++ Connection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+
+namespace MultiSSH
+{
+    public sealed class Connection : IDisposable
+    {
+        public string HostName { get; set; } = null;
+        public string Username { get; set; } = null;
+        public string Password { get; set; } = null;
+        public int? Port { get; set; } = null;
+
+        private SshClient Client { get; set; }
+
+        public Connection(string hostName, string username, string password = null, int? port = null)
+        {
+            HostName = hostName;
+            Username = username;
+            Password = password;
+            Port = port;
+
+            AuthenticationMethod auth = Password switch
+            {
+                null => new PasswordAuthenticationMethod(Username, Password),
+                _ => new NoneAuthenticationMethod(Username),
+            };
+
+            ConnectionInfo connectionInfo = new(HostName, Port ?? 22, Username, auth);
+
+            Client = new SshClient(connectionInfo);
+        }
+
+        public void Connect() => Client.Connect();
+        public void Disconnect() => Client.Disconnect();
+        public void Dispose() => Client.Dispose();
+        public void RunCommand(string command) => Client.RunCommand(command);
+
+    }
+}
